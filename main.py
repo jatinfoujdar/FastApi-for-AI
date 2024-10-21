81693d30c9ae1baa153acfcada1920368f002ee5
Jatin Foujdar <jatin321001@gmail.com>
2024-10-21 16:56:40 +0530

    in memory tweets coomits

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from fastapi import FastAPI
+import time
+
+
+# in memory tweets
+tweets = [
+  {
+    "timestamp": "20240929101921",
+    "tweet": "Hello AI",
+    "user_name": "c17hawke"
+  },
+  {
+    "timestamp": "20240929101930",
+    "tweet": "Hello world!",
+    "user_name": "c17hawke"
+  },
+  {
+    "timestamp": "20240929102013",
+    "tweet": "Its an amazing day!",
+    "user_name": "User1"
+  }
+]
 
 app = FastAPI()
 
